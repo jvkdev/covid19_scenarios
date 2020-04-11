bcd64f6dcd789030698a769a986373d9873fe4c1
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-04-12 01:40:49 +0200

    DONOTMERGE: tex

--- src/pages/Faq.tsx
+++ src/pages/Faq.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { Col, Container, Row } from 'reactstrap'
 
+import { InlineMath, BlockMath } from 'react-katex'
+
 import FaqTableOfContents from '../components/Faq/FaqTableOfContents'
 
 import FaqContent from '../assets/text/faq.mdx'
@@ -9,6 +11,8 @@ import FaqContent from '../assets/text/faq.mdx'
 export default function Faq() {
   return (
     <Container>
+      <InlineMath math={`M_{0}^{a}`} />
+      <BlockMath math={`M_{0}^{a}`} />
       <Row>
         <Col>
           <h1 className="h1-default">Frequently asked questions</h1>
